79a6c41b36ddb917d9ba72e43d73f62f13091310
Gabriel Lima <gabriellimatrajano@hotmail.com>
2018-01-18 23:47:24 -0200

    vai

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :skip => [:registrations]}
-  # devise_for :users, controllers: {
-  #       sessions: 'users/sessions'
-  #     }
-  #   end
+  devise_for :users
   root 'home#index'
-  # as :user do
-  #   get "/registrations", to: "registrations#new", as: "register"
-  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
