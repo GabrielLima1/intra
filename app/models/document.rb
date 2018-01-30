5ea39978c05f773cbe8e9b87ad8f06ed4b0e8160
Gabriel Lima <gabriellimatrajano@hotmail.com>
2018-01-30 19:12:21 -0200

    subindo alters simples

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,11 @@
 class Document < ApplicationRecord
   validates_presence_of :title, :file, :user_id, message: "Ops! Preencha esse Aqui!"
   belongs_to :user
-  
   mount_uploader :file, FileUploader
+
+  after_create :email_admin_user
+
+  def email_admin_user
+    UserMailer.email_admin_user.deliver
+  end
 end
