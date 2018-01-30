7197466aeb6d4206c324bdc9663f7d1865f3bef2
Gabriel Lima <gabriellimatrajano@hotmail.com>
2018-01-30 18:43:05 -0200

    subindo alters simples

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     controllers: {:registrations => "registrations"}
   resources :users, only: [:index]
   root 'home#index'
+  #root 'documents#index'
   resources :documents
   resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
